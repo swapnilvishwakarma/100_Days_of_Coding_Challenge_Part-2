2a6bb9ea460aae8000372007751fcdb8122863e3
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-05-16 09:53:44 +0530

    Day36 Count Primes

---
+++ 36.Count_Primes.py
@@ -0,0 +1,15 @@
+# Count the number of prime numbers less than a non-negative number, n.
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+        
+        numbers = {}
+        for p in range(2, int(sqrt(n)) + 1):
+            if p not in numbers:
+                for multiple in range(p*p, n, p):
+                    numbers[multiple] = 1
+        
+        # Exclude "1" and the number "n" itself.
+        return n - len(numbers) - 2
