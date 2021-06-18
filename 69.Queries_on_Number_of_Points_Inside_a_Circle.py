d4e851c66ad5593f99a8325bf7f9508f64a713bf
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-06-18 11:27:18 +0530

    Day69 Queries on Number of Points Inside a Circle

--- 69.Queries_on_Number_of_Points_Inside_a_Circle.py
+++ 69.Queries_on_Number_of_Points_Inside_a_Circle.py
@@ -3,13 +3,15 @@
 # For each query queries[j], compute the number of points inside the jth circle. Points on the border of the circle are considered inside.
 # Return an array answer, where answer[j] is the answer to the jth query.
 
+import math
+
 class Solution:
     def countPoints(self, points: list, queries: list) -> list:
         result = []
         for q in queries:
             count = 0
             for p in points:
-                ed = sqrt( (p[0] - q[0])**2 + (p[1] - q[1])**2 )
+                ed = math.sqrt( (p[0] - q[0])**2 + (p[1] - q[1])**2 )
                 if ed <= q[2]:
                     count += 1
             result.append(count)
