a1d3c66e152bf6000638a6345fec7c96718d44e6
Swapnil Vishwakarma <swapnil.vishwakarma18@siesgst.ac.in>
2021-07-09 11:14:45 +0530

    Day90 Redundant Connection

---
+++ 90.Redundant_Connection.py
@@ -0,0 +1,36 @@
+# In this problem, a tree is an undirected graph that is connected and has no cycles.
+# You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
+# Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
+
+
+class Solution:
+    def findRedundantConnection(self, edges: list) -> list:
+        parent = [i for i in range(len(edges)+1)]
+        rank = [1] * (len(edges)+1)
+        
+        def find(n):
+            p = parent[n]
+            while p != parent[p]:
+                parent[p] = parent[parent[p]]
+                p = parent[p]
+            return p
+        
+        # Return False if can't complete
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            
+            if p1 == p2:
+                return False
+            
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] += rank[p2]    
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            
+            return True
+        
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return [n1, n2]
